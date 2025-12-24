d707c996e3cac5806c8877d66335f4224b16487f
MHaroonPC <mharoonyaqubi@gmail.com>
2025-12-24 12:54:54 +0100

    add the test for the backend 2

--- backend/complice_taches/urls.py
+++ backend/complice_taches/urls.py
@@ -2,22 +2,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 # Test view
-def test_view(request):
-    return HttpResponse("Test endpoint works!", content_type="text/plain")
+def user_test(request):
+    return HttpResponse("User API is working!", content_type="text/plain")
 
 urlpatterns = [
-    # Test endpoints
-    path('', test_view, name='root'),
-    path('test/', test_view, name='test'),
-    
-    # Your existing endpoints
-    path('admin/', admin.site.urls),
-    path('api/', include('todolist_app.urls')),
+    # Test direct path (bypassing users_app.urls)
+    path('api/user/test/', user_test, name='user-test'),
+
+    # Your existing try
     path('api/user/', include('users_app.urls')),
-    path('api-auth/', include('rest_framework.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # ... rest of your URLs
 ]

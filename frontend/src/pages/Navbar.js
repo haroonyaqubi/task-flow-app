8511a9c927838519dac332999d4e7f738ca2ff48
MHaroonPC <mharoonyaqubi@gmail.com>
2025-12-24 12:04:07 +0100

    the navbar page code is updated

--- frontend/src/pages/Navbar.js
+++ frontend/src/pages/Navbar.js
@@ -1,32 +1,57 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import logo from '../images/logo.png';
 
 function Navbar() {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem('access');
+    localStorage.removeItem('refresh');
+    navigate('/login');
+  };
+  const isLoggedIn = !!localStorage.getItem('access');
+
   return (
-      <nav className="navbar">
-        <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
-            TaskFlow
+      <nav className="navbar navbar-expand-lg" style={{ backgroundColor: '#7C3AED' }}>
+        <div className="container">
+          <Link className="navbar-brand d-flex align-items-center text-white" to="/">
+            <img src={logo} alt="Logo" width="40" height="40" className="me-2" />
           </Link>
-
-          <div className="navbar-menu">
-            <Link to="/" className="navbar-item">
-              Home
-            </Link>
-            <Link to="/tasks" className="navbar-item">
-              My Tasks
-            </Link>
-            <Link to="/create" className="navbar-item">
-              Create Task
-            </Link>
-            <Link to="/profile" className="navbar-item">
-              Profile
-            </Link>
-          </div>
-
-          <div className="navbar-auth">
-            <button className="btn btn-login">Login</button>
-            <button className="btn btn-signup">Sign Up</button>
+          <button
+              className="navbar-toggler bg-light"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item"><Link className="nav-link text-white" to="/">Accueil</Link></li>
+              <li className="nav-item"><Link className="nav-link text-white" to="/taches">Tâches</Link></li>
+              <li className="nav-item"><Link className="nav-link text-white" to="/contact">Contact</Link></li>
+              <li className="nav-item"><Link className="nav-link text-white" to="/apropos">À propos</Link></li>
+            </ul>
+            <ul className="navbar-nav ms-auto">
+              {!isLoggedIn && (
+                  <li className="nav-item">
+                    <Link className="nav-link text-white" to="/login">Connexion</Link>
+                  </li>
+              )}
+              <li className="nav-item">
+                <Link className="nav-link text-white" to="/register">S'inscrire</Link>
+              </li>
+              {isLoggedIn && (
+                  <li className="nav-item">
+                    <button className="btn btn-outline-light ms-2" onClick={handleLogout}>
+                      Déconnexion
+                    </button>
+                  </li>
+              )}
+            </ul>
           </div>
         </div>
       </nav>
